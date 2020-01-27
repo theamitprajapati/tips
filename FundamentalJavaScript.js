91fe9448119ec137bf3e192b1ac367c65e8c922b
Amit Prajapati <52284954+theamitprajapati@users.noreply.github.com>
2020-01-27 10:27:44 +0530

    Create FundamentalJavaScript.js

---
+++ FundamentalJavaScript.js
@@ -0,0 +1,28 @@
+Introduction and motivation
+Writing your first program
+Variables
+Types
+Conditionals
+Conditionals
+Looping
+Nested loops
+Functions
+Hoisting
+Recursive functions
+Objects
+Object vs Functions
+Constructor function
+Context and Scope
+Understanding “this"
+Closures
+DOM
+Events
+Concepts of virtual DOM
+Shadow DOM
+Asynchronous code
+Promise
+Forms and best practices
+Browser history
+Frames
+Ajax and related concepts
+HTML5
