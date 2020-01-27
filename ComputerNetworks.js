b763d0bf8b303acd5badf744e2f4f25410983033
Amit Prajapati <52284954+theamitprajapati@users.noreply.github.com>
2020-01-27 10:45:20 +0530

    Create ComputerNetworks.js

---
+++ ComputerNetworks.js
@@ -0,0 +1,15 @@
+Introduction to Computer Networks
+Transmission Modes
+Network Topologies
+TCP/IP vs OSI Model
+Circuit Switching vs Packet Switching
+Flow Control Protocols
+Error Detection
+IP and Classful Addressing
+Classless Addressing
+IPv4 vs IPv6
+Routing Protocols
+ARP & Reverse ARP
+Transport Layer
+TCP & UDP
+Application Layer
