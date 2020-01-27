0c13ee538b5d76e82e93ac321185b225b5ac449d
Amit Prajapati <52284954+theamitprajapati@users.noreply.github.com>
2020-01-27 11:45:40 +0530

    Create ReactJS.js

---
+++ ReactJS.js
@@ -0,0 +1,28 @@
+Introduction to the philosophy
+The Virtual DOM
+Build your own Virtual Dom
+Understanding JSX
+Conditionals and Loops
+Components
+Functional and Stateful Component
+Parent/Child communication
+Understanding state and props
+Asynchronous nature of setState()
+Life Cycle
+Children types
+The Context API
+Destructuring arguments
+Spread attributes
+Style Components
+Controlled Input
+Presentation and Container Component
+Higher-Order Components
+The Render Props
+State Management
+Building reusable components
+Authentication and Authorization
+Animations and Transitions
+Server-Side Rendering
+Application Architecture
+Server-Side Rendering
+Performance Best Practices (Learning from the field)
