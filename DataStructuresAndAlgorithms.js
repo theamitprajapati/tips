934d5d8ca6b732944738a48df3a81c489104e2d4
Amit Prajapati <52284954+theamitprajapati@users.noreply.github.com>
2020-01-27 10:36:36 +0530

    Create DataStructuresAndAlgorithms.js

---
+++ DataStructuresAndAlgorithms.js
@@ -0,0 +1,9 @@
+Array
+Basic Recursion
+Hashing
+String
+Searching (Linear Search, Binary Search)
+Sorting ( QuickSort and its variation, Mergesort, Counting sort, Insertion Sort, Heap Sort, Comparator)
+Linked List ( Singly Linked List, Doubly Linked Lists, Circular Linked List, Skip List, Doubly Circular)
+Stack
+Queue
