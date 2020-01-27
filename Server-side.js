7cb0f123cffdf0a638bd8f62cc070c825481f717
Amit Prajapati <52284954+theamitprajapati@users.noreply.github.com>
2020-01-27 10:57:55 +0530

    Create Server-side.js

---
+++ Server-side.js
@@ -0,0 +1,10 @@
+Concepts and foundations of NodeJS
+Understanding core packages
+Understanding Routing
+Understanding SSR
+Understanding session and state management
+Authentication and Authorization
+Single sign-on
+Security
+Socket.IO for real-time communication
+Deployment
